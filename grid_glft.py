2a91c985174b5dad534a71495a677cdd61380dbb
夏西西 <81393403@qq.com>
2025-10-22 12:42:51 +0800

    feat

--- grid_glft.py
+++ grid_glft.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 from numba import int64, float64
 from numba.experimental import jitclass
 # 定义类型规格
@@ -28,11 +28,14 @@ class StreamingMean:
     @property
     def mean(self) -> float:
         return 0.0 if self.n == 0 else (self._sum + self._comp) / self.n
-#%%
+
+# %%
 from hftbacktest import BUY, SELL, GTX, LIMIT, BUY_EVENT
 from numba.typed import Dict
 from numba import uint64,njit
 import numpy as np
+from datetime import datetime, timezone
+
 
 @njit
 def measure_trading_intensity(order_arrival_depth, out):
@@ -57,48 +60,6 @@ def measure_trading_intensity(order_arrival_depth, out):
 
         max_tick = max(max_tick, tick)
     return out[:max_tick]
-@njit
-def measure_trading_intensity_and_volatility(hbt):
-    tick_size = hbt.depth(0).tick_size
-    arrival_depth = np.full(10_000_000, np.nan, np.float64)
-    mid_price_chg = np.full(10_000_000, np.nan, np.float64)
-
-    t = 0
-    prev_mid_price_tick = np.nan
-    mid_price_tick = np.nan
-
-    # Checks every 100 milliseconds.
-    while hbt.elapse(100_000_000) == 0:
-        #--------------------------------------------------------
-        # Records market order's arrival depth from the mid-price.
-        if not np.isnan(mid_price_tick):
-            depth = -np.inf
-            for last_trade in hbt.last_trades(0):
-                trade_price_tick = last_trade.px / tick_size
-
-                if last_trade.ev & BUY_EVENT == BUY_EVENT:
-                    depth = np.nanmax([trade_price_tick - mid_price_tick, depth])
-                else:
-                    depth = np.nanmax([mid_price_tick - trade_price_tick, depth])
-            arrival_depth[t] = depth
-
-        hbt.clear_last_trades(0)
-
-        depth = hbt.depth(0)
-
-        best_bid_tick = depth.best_bid_tick
-        best_ask_tick = depth.best_ask_tick
-
-        prev_mid_price_tick = mid_price_tick
-        mid_price_tick = (best_bid_tick + best_ask_tick) / 2.0
-
-        # Records the mid-price change for volatility calculation.
-        mid_price_chg[t] = mid_price_tick - prev_mid_price_tick
-
-        t += 1
-        if t >= len(arrival_depth) or t >= len(mid_price_chg):
-            raise Exception
-    return arrival_depth[:t], mid_price_chg[:t]
 
 @njit
 def linear_regression(x, y):
@@ -119,7 +80,7 @@ def compute_coeff(xi, gamma, delta, A, k):
     return c1, c2
 
 @njit
-def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 = 0.05):
+def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1.0 ,adj1 = 1.0 ,adj2 = 0.05, fit_window = 600, bins_for_fit = 70):
     asset_no = 0
     tick_size = hbt.depth(asset_no).tick_size
 
@@ -136,19 +97,17 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
     A = np.nan
     k = np.nan
     volatility = np.nan
-    # gamma = 0.05
-    # delta = 1
-    # adj1 = 1
-    # adj2 = 0.05
 
-    order_qty = 1
+
+    order_qty = 0.5
     max_position = 200
     grid_num = 20
 
     avg = StreamingMean()
 
-    # Checks every 100 milliseconds.
+    # Checks every 1 second.
     while hbt.elapse(1_000_000_000) == 0:
+
         #--------------------------------------------------------
         # Records market order's arrival depth from the mid-price.
         if not np.isnan(mid_price_tick):
@@ -167,7 +126,6 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
 
         depth = hbt.depth(asset_no)
         position = hbt.position(asset_no)
-        avg.push(position)
 
         orders = hbt.orders(asset_no)
 
@@ -184,14 +142,14 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
         # Calibrates A, k and calculates the market volatility.
 
         # Updates A, k, and the volatility every 5-sec.
-        if t % 10 == 0:
+        if t % 5 == 0:
             # Window size is 10-minute.
-            if t >= 600 - 1:
+            if t >= fit_window - 1:
                 # Calibrates A, k
                 tmp[:] = 0
-                lambda_ = measure_trading_intensity(arrival_depth[t + 1 - 6_000:t + 1], tmp)
+                lambda_ = measure_trading_intensity(arrival_depth[t + 1 - fit_window:t + 1], tmp)
                 if len(lambda_) > 2:
-                    lambda_ = lambda_[:25] / 600
+                    lambda_ = lambda_[:bins_for_fit] / fit_window
                     x = ticks[:len(lambda_)]
                     y = np.log(lambda_)
                     k_, logA = linear_regression(x, y)
@@ -199,7 +157,12 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
                     k = -k_
 
                 # Updates the volatility.
-                volatility = np.nanstd(mid_price_chg[t + 1 - 6_000:t + 1]) * np.sqrt(10)
+                volatility = np.nanstd(mid_price_chg[t + 1 - fit_window:t + 1]) * np.sqrt(10)
+        
+        if abs(k) < 1e-9 or abs(A) < 1e-9:
+            t += 1
+            return
+        
 
         #--------------------------------------------------------
         # Computes bid price and ask price.
@@ -222,10 +185,11 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
         bid_price = np.floor(bid_price / grid_interval) * grid_interval
         ask_price = np.ceil(ask_price / grid_interval) * grid_interval
 
+
         #--------------------------------------------------------
         # Updates quotes.
 
-        # Creates a new grid for buy orders.
+        # # Creates a new grid for buy orders.
         new_bid_orders = Dict.empty(np.uint64, np.float64)
         if position < max_position and np.isfinite(bid_price):
             for i in range(grid_num):
@@ -281,58 +245,73 @@ def gridtrading_glft_mm(hbt, recorder, gamma = 0.05, delta = 1 ,adj1 = 1 ,adj2 =
 
 
     return avg.mean
-#%%
+
+# %%
 from hftbacktest.stats import LinearAssetRecord
 from hftbacktest import Recorder, BacktestAsset, ROIVectorMarketDepthBacktest
 
+data = np.concatenate(
+[np.load('C:/Users/81393/Desktop/code/hfbacktest/data/bitget_spot/{}/SPOT.SOLUSDT.npz'.format(date))['data'] for date in [20251020, 20251021, 20251022]]
+)
+initial_snapshot = np.load('C:/Users/81393/Desktop/code/hfbacktest/data/bitget_spot/20251019/SPOT.SOLUSDT.eod.npz')['data']
+latency_data = np.concatenate(
+[np.load('C:/Users/81393/Desktop/code/hfbacktest/data/bitget_spot/{}/SPOT.SOLUSDT.latency.npz'.format(date))['data'] for date in [20251020, 20251021, 20251022]]
+)
+
 asset = (
     BacktestAsset()
-        .data([
-            'data/binance_spot/solusdt_20251011.npz',
-            'data/binance_spot/solusdt_20251012.npz',
-            'data/binance_spot/solusdt_20251013.npz',
-            'data/binance_spot/solusdt_20251014.npz',
-            'data/binance_spot/solusdt_20251015.npz',
-        ])
-        .initial_snapshot('data/binance_spot/solusdt_20251010_eod.npz')
+        .data(data)
+        .initial_snapshot(initial_snapshot)
         .linear_asset(1.0)
-        .intp_order_latency([
-            'data/binance_spot/solusdt_20251011_latency.npz',
-            'data/binance_spot/solusdt_20251012_latency.npz',
-            'data/binance_spot/solusdt_20251013_latency.npz',
-            'data/binance_spot/solusdt_20251014_latency.npz',
-            'data/binance_spot/solusdt_20251015_latency.npz',
-        ])
+        .intp_order_latency(latency_data)
         .power_prob_queue_model(2.0)
         .no_partial_fill_exchange()
         .trading_value_fee_model(-0.5/1e4, 3/1e4)
         .tick_size(0.01)
-        .lot_size(0.001)
+        .lot_size(0.0001)
         .roi_lb(0.0)
-        .roi_ub(3000.0)
+        .roi_ub(500.0)
         .last_trades_capacity(10000)
 )
 
 
-#%%
-import optuna
-def objective(trail:optuna.Trial):
-    gamma = trail.suggest_float('gamma', 0.01, 1.0, step=0.01)
-    delta = trail.suggest_float('delta', 0.1, 10, step=0.1)
-    adj1 = trail.suggest_float('adj1', 0.1, 10, step=0.1)
-    adj2 = trail.suggest_float('adj2', 0.01, 1.0, step=0.01)
 
+# %%
+import optuna
+def objective(trail:optuna.Trial| None = None):
+    if trail:
+        gamma = trail.suggest_float('gamma', 0.01, 1.0, step=0.01)
+        delta = trail.suggest_float('delta', 0.1, 10, step=0.1)
+        adj1 = trail.suggest_float('adj1', 0.01, 10, step=0.01)
+        adj2 = trail.suggest_float('adj2', 0.01, 1.0, step=0.01)
+        fit_window = trail.suggest_int('update_A_K_window', 100, 3000, step=100)
+        bins_for_fit = trail.suggest_int('bins_for_fit', 40, 120, step=10)
+    else:
+    # 1231 [0.17404209333333825, 13984.706214662881, 44265.853372434016, 1.717052732784075] {'gamma': 0.04, 'delta': 4.1, 'adj1': 0.1, 'adj2': 0.04, 'update_A_K_window': 1700, 'bins_for_fit': 60}
+    # 26922 [0.14802678166655256, 7778.96604340188, 24660.457478005865, 3.227600182861747] {'gamma': 0.04, 'delta': 9.3, 'adj1': 0.1, 'adj2': 0.15000000000000002, 'update_A_K_window': 1300, 'bins_for_fit': 50}
+
+        gamma = 0.04
+        delta = 4.1
+        adj1 = 0.1
+        adj2 = 0.04
+        fit_window = 1700
+        bins_for_fit = 60
     hbt = ROIVectorMarketDepthBacktest([asset])
     recorder = Recorder(1, 5_000_000)
-    avg = gridtrading_glft_mm(hbt, recorder.recorder, gamma, delta, adj1, adj2)
+    avg = gridtrading_glft_mm(hbt, recorder.recorder, gamma, delta, adj1, adj2, fit_window, bins_for_fit) 
 
     hbt.close()
     stats = LinearAssetRecord(recorder.get(0)).stats(book_size=30_000)
-    return stats.splits[0]['Return'] * 1e4, stats.splits[0]['DailyNumberOfTrades'] if  stats.splits[0]['DailyNumberOfTrades'] > abs(avg) else -stats.splits[0]['DailyNumberOfTrades'] , -abs(avg), -stats.splits[0]['MaxDrawdown'] * 1e4
-#%%
-study = optuna.create_study(study_name="mm-grid-glft-solusdt-No@1",
+
+    return stats.splits[0]['Return'] * 1e2, stats.splits[0]['DailyTurnover'] * 1e2  ,stats.splits[0]['DailyNumberOfTrades'] / abs(avg or 1), stats.splits[0]['ReturnOverMDD'] if not np.isnan(stats.splits[0]['ReturnOverMDD']) else 0
+    # return stats
+
+# %%
+study = optuna.create_study(study_name="mm-grid-glft-solusdt-bitget@No.6",
                             directions=["maximize", "maximize", "maximize", "maximize"],
                             sampler=optuna.samplers.TPESampler(seed=42),
                             storage="mysql://optuna:AyHfbtAyAiRjR4ck@47.86.7.11/optuna",
                             load_if_exists=True)
 study.optimize(objective, n_trials=int(3 * 1e3), n_jobs=1)
+
+
